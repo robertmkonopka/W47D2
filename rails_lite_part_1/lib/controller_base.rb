1527fe68243544b4d44aed735087134ef40f60d8
robertmkonopka <robertmakoto@mac.com>
2021-02-02 10:31:41 -0800

    Finished Phase 3

--- rails_lite_part_1/lib/controller_base.rb
+++ rails_lite_part_1/lib/controller_base.rb
@@ -42,6 +42,18 @@ class ControllerBase
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(template_fname)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
   end
 
   # method exposing a `Session` object
