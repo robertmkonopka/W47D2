398070d93e78233f5062ebda220a056551d27db8
robertmkonopka <robertmakoto@mac.com>
2021-02-02 17:16:42 -0800

    Finished Phase 8

---
+++ rails_lite_part_2/lib/controller_base.rb
@@ -0,0 +1,94 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+require_relative './flash'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res, route_params = {})
+    @req, @res = req, res
+    @params = route_params.merge(req.params)
+    @already_built_response = false
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+    @already_built_response
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    prepare_render_or_redirect
+
+    @res.status = 302
+    @res["Location"] = url
+
+    nil
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type)
+    prepare_render_or_redirect
+
+    @res.write(content)
+    @res['Content-Type'] = content_type
+
+    nil
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
+
+    template_code = File.read(template_fname)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
+  end
+
+  # method exposing a `Session` object
+  def session
+    @session ||= Session.new(@req)
+  end
+
+  def flash
+    @flash ||= Flash.new(@req)
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+    self.send(name)
+    render(name) unless already_built_response?
+
+    nil
+  end
+
+  private
+
+  attr_accessor :already_built_response
+
+  def prepare_render_or_redirect
+    raise "double render error" if already_built_response?
+    @already_built_response = true
+    session.store_session(@res)
+    flash.store_flash(@res)
+  end
+
+  def controller_name
+    self.class.to_s.underscore
+  end
+
+end
+
